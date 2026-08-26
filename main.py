df26a09a3a4db511466fefe2eab0e994de483a61
DeLara9S-ay2526 <delara.xq@obmontessori.edu.ph>
2026-08-26 22:54:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyscript import display, document
 # Drill 2
 
 
+# Adds the two values
 def addition(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
@@ -10,6 +11,7 @@ def addition(e):
 
     display(f'The sum of {num_1} and {num_2} is {sum}.', target='result')
 
+# Subtracts the two values
 def subtraction(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
@@ -18,6 +20,7 @@ def subtraction(e):
 
     display(f'The difference of {num_1} and {num_2} is {diff}.', target='result')
 
+# Multiplies the two values
 def multiply(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
@@ -26,6 +29,7 @@ def multiply(e):
 
     display(f'The product of {num_1} and {num_2} is {prod}.', target='result')
 
+# Divides the two values
 def divide(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
@@ -34,6 +38,7 @@ def divide(e):
 
     display(f'The quotient of {num_1} and {num_2} is {quot}.', target='result')
 
+# Finds the remainder between the two values when divided
 def modulate(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
@@ -42,6 +47,7 @@ def modulate(e):
 
     display(f'The remainder of {num_1} and {num_2} is {rem}.', target='result')
 
+# Raises the first value to the power of the second
 def exponent(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
@@ -50,6 +56,7 @@ def exponent(e):
 
     display(f'{num_1} to the power of {num_2} is {pow}.', target='result')
 
+# Rounds off the result of the two values when divided
 def floor(e):
     document.getElementById('result').innerHTML = " "
     num_1 = float(document.getElementById('num1').value)
